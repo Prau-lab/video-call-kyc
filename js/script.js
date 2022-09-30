10a36d2f63962c5e2599c561c9dcbd671d522169
Prau-lab <prayushthapa12@gmail.com>
2022-09-30 22:00:10 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function show() {
+    var icons = document.getElementById('none-section');
+    if(icons.style.display == "block"){
+        icons.style.display = "none";
+    }
+    else{
+        icons.style.display = "block";
+    }
+}
+
+
+
